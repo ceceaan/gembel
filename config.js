7b415734070961dc1cd2a685ca8fd683729bb226
ceceaan <dimaswanggaa@gmail.com>
2026-04-24 12:51:49 +0700

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // ISI DATA KAMU DI SINI
+    ADMIN_IDS: [8390366622], // ID Admin
+    
+    BOT_TOKEN: "8741403275:AAHvc2GyQWAghv-NC4dHIr7zS9tUF0UptKo", // Token dari @BotFather
+    
+    GROUP_NAME: "Premium Apps Store",
+
+    BAD_WORDS: ["anjing", "babi", "jancok", "asu", "kontol", "memek", "bangsat"],
+    TYPO_COMMANDS: ["/pice", "/pri", "/rpice", "/prce", "/startt"]
+};
